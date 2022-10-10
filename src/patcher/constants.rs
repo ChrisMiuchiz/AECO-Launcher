bfbc8e220ee18b7d70396a63b6e4b1da39361bd1
ChrisMiuchiz <chris000005@gmail.com>
2022-10-09 20:37:28 -0400

    Change base zip to be saga10

--- src/patcher/constants.rs
+++ src/patcher/constants.rs
@@ -1,6 +1,6 @@
 pub const PATCH_SERVER: &str = "http://127.0.0.1:8000/";
 pub const BASE_DIR: &str = "base/";
-pub const BASE_ZIP: &str = "saga18.zip";
+pub const BASE_ZIP: &str = "saga10.zip";
 pub const META_DIR: &str = "meta/";
 pub const PATCHLIST: &str = "patchlist.json";
 pub const STATUS: &str = "status.json";
